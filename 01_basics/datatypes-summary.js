639880b24a80b3a2bb5c4ce973940c4ed6d90eb8
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2024-06-09 15:31:59 +0000

    heap and stack

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,7 +11,7 @@ let userEmail;
 let id = Symbol('123')
 let anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -30,3 +30,28 @@ const myfunciton = function()
     console.log("Hello World");
 }
 
+//---------------------------------------------------------------------------------------------
+
+// STACK (Primitive),  HEAP (Non-Primitive)
+
+// STACK -> we get the copy of the data
+// HEAP -> we get the original data
+
+let myName = "Ironman"
+
+let anothername = myName
+anothername = "Tony Stark"
+
+console.log(myName);
+console.log(anothername);
+
+let userOne = 
+    {
+    email : "usernamegmail.com",
+    location : "Delhi"
+    }
+
+let userTwo = userOne
+
+userTwo.email = "shivam@gmail.com"
+console.log(userOne.email);
