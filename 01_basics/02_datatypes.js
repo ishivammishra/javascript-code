942d9f4d288ac12bc8ae6c52171d4f764feb3704
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2024-06-09 15:36:48 +0000

    renamed the file

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Shivam")
+
+
+let name = "shivam"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => datatype is declared but it is not defined yet
+// symbol => uniqueness
+
+console.log(typeof undefined); // output come to be -> undefined
+console.log(typeof null); // output comes to be -> object
