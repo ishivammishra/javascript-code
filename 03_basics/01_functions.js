1d18411606de672918a45b98812c51545f1bd381
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2024-06-24 16:32:18 +0000

    functions were discussed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,32 @@ function calculateCartPrice(val1, val2, ...num1) {
   return num1;
 }
 let price = calculateCartPrice(200, 400, 500, 2000)
-console.log(price);
+// console.log(price);
+
+const user = 
+{
+  username: "shivam",
+  price: 199
+}
+
+function handleObject(anyobject)
+{
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+  username: "ram",
+  price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray)
+{
+  return getArray[3]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
+
