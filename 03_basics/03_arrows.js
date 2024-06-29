17e9ab8427f67d884a310935c1d8259c682fdd19
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2024-06-29 15:37:58 +0000

    arrow function

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -1,10 +1,43 @@
 const user = {
-    username: "hitesh",
-    price: 999,
+    username: "Shivam",
+    price: 1000,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        // console.log(`${this.username} , welcome to website`);
+        // console.log(this);
     }
 
 }
+
+// user.welcomeMessage()
+// user.username = "Mishra"
+// user.welcomeMessage()
+// console.log(this);
+
+// function coffee()
+// {
+//     let username = "Shivam"
+//     console.log(this.username);
+// }
+
+// coffee()
+
+const coffee = () => {
+    let username = "Shivam"
+    console.log(this);
+}
+
+// coffee()
+
+// -----------------curly braces use huua to return keyword likhna padega-------- 
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)
+const addTwo = (num1, num2) => ({username: "Shivam"})
+
+console.log(addTwo(3, 4));
+
